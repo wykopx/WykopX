52e45c59aa09a2d5160c9e748dedad26ef357fe7
Wykop X <wykopx@gmail.com>
2024-05-12 02:00:34 +0200

    Wykop XS 3 - 3.0.44

--- Wykop XS/wykop_xs.user.js
+++ Wykop XS/wykop_xs.user.js
@@ -32,7 +32,6 @@
 // @require							https://unpkg.com/localforage@1.10.0/dist/localforage.min.js
 // @require							https://cdn.jsdelivr.net/npm/dayjs@1.11.10/dayjs.min.js
 // @require							https://cdn.jsdelivr.net/npm/dayjs@1.11.10/plugin/relativeTime.js
-// @require							https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@latest/waitForKeyElements.js
 
 
 
@@ -9822,3 +9821,87 @@ Liczba zakopujących: ${link_data.votes.down} (${link_data.votes.votesDownPercen
 	// head.insertAdjacentHTML("beforeend", CSS);
 
 })();
+
+
+
+
+
+
+// https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@latest/waitForKeyElements.js
+/**
+ * A utility function for userscripts that detects and handles AJAXed content.
+ *
+ * @example
+ * waitForKeyElements("div.comments", (element) => {
+ *   element.innerHTML = "This text inserted by waitForKeyElements().";
+ * });
+ *
+ * waitForKeyElements(() => {
+ *   const iframe = document.querySelector('iframe');
+ *   if (iframe) {
+ *     const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+ *     return iframeDoc.querySelectorAll("div.comments");
+ *   }
+ *   return null;
+ * }, callbackFunc);
+ *
+ * @param {(string|function)} selectorOrFunction - The selector string or function.
+ * @param {function}          callback           - The callback function; takes a single DOM element as parameter.
+ *                                                 If returns true, element will be processed again on subsequent iterations.
+ * @param {boolean}           [waitOnce=true]    - Whether to stop after the first elements are found.
+ * @param {number}            [interval=300]     - The time (ms) to wait between iterations.
+ * @param {number}            [maxIntervals=-1]  - The max number of intervals to run (negative number for unlimited).
+ */
+function waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals)
+{
+	if (typeof waitOnce === "undefined")
+	{
+		waitOnce = true;
+	}
+	if (typeof interval === "undefined")
+	{
+		interval = 300;
+	}
+	if (typeof maxIntervals === "undefined")
+	{
+		maxIntervals = -1;
+	}
+	if (typeof waitForKeyElements.namespace === "undefined")
+	{
+		waitForKeyElements.namespace = Date.now().toString();
+	}
+	var targetNodes = (typeof selectorOrFunction === "function")
+		? selectorOrFunction()
+		: document.querySelectorAll(selectorOrFunction);
+
+	var targetsFound = targetNodes && targetNodes.length > 0;
+	if (targetsFound)
+	{
+		targetNodes.forEach(function (targetNode)
+		{
+			var attrAlreadyFound = `data-userscript-${waitForKeyElements.namespace}-alreadyFound`;
+			var alreadyFound = targetNode.getAttribute(attrAlreadyFound) || false;
+			if (!alreadyFound)
+			{
+				var cancelFound = callback(targetNode);
+				if (cancelFound)
+				{
+					targetsFound = false;
+				}
+				else
+				{
+					targetNode.setAttribute(attrAlreadyFound, true);
+				}
+			}
+		});
+	}
+
+	if (maxIntervals !== 0 && !(targetsFound && waitOnce))
+	{
+		maxIntervals -= 1;
+		setTimeout(function ()
+		{
+			waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals);
+		}, interval);
+	}
+}
