b09be2a7bff5f2cb7161751b489a6c8a9c3be37b
Wykop X <wykopx@gmail.com>
2024-06-07 01:51:44 +0200

    XS 3.0.60

--- Wykop XS/wykop_xs.user.js
+++ Wykop XS/wykop_xs.user.js
@@ -3,7 +3,7 @@
 // @name:pl							Wykop XS 3.0
 // @name:en							Wykop XS 3.0
 
-// @version							3.0.59
+// @version							3.0.60
 
 // @description 					Wykop XS służy do wspomagania działania stylu "Wykop X Style 3", który jest sugerowany do poprawnego działania niniejszego skryptu. Wykop X Style znajdziesz na http://styl.wykopx.pl
 // @description:en 					Wykop XS is a helper script for userstyle "Wykop X Style 3" which modifies wykop.pl website and make it easier to use adding enhancements and new features. Check it out here: http://styl.wykopx.pl
@@ -46,7 +46,7 @@
 	'use strict';
 
 
-	const currentVersion = "3.0.59";
+	const currentVersion = "3.0.60";
 	let dev = false;
 
 	const promoString = " [Dodane przez Wykop XS]";
